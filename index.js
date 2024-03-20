49b67aa918c5a8facc430a8d69b6ec4c2175975e
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-03-20 13:56:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callback){
+callback();
+}
+
+function returnsANamedFunction(){
+    function namedFunction(){
+        return `This is a named function`;
+    }
+    return namedFunction;
+}
+
+function returnsAnAnonymousFunction(){
+    
+        return function(){
+            return `This is an anonymous function`
+        };
+    }
+    const myFunction = returnsAnAnonymousFunction();
+    myFunction();
